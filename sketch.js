bf7b6f03b12c98ba5a6bf0f4f5f9f958f02b70fa
vicwski <90584286+vicwski@users.noreply.github.com>
2022-08-29 15:29:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,10 +7,22 @@ var distance = 0
 var database
 
 var form, player, game
+
 var cars, car1, car2, car3, car4
 
+var track, car1_img, car2_img, car3_img, car4_img
+
+function preload() {
+  track = loadImage('../images/track.jpg')
+  car1_img = loadImage('../images/car1.png')
+  car2_img = loadImage('../images/car2.png')
+  car3_img = loadImage('../images/car3.png')
+  car4_img = loadImage('../images/car4.png')
+  ground = loadImage('../images/ground.png')
+}
+
 function setup() {
-  canvas = createCanvas(displayWidth, displayHeight)
+  canvas = createCanvas(displayWidth - 20, displayHeight - 30)
   database = firebase.database()
   game = new Game()
   game.getState()
@@ -24,5 +36,8 @@ function draw() {
   if (gameState === 1) {
     clear()
     game.play()
+  }
+  if (gameState === 2) {
+    game.end()
   }
 }
